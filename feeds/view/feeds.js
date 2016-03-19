6229751a7d27013326fd3b80a75274316f2de6b3
筷子 <season.chopsticks@gmail.com>
2016-03-19 18:38:46 +0800

    Add feeds page

---
+++ feeds/view/feeds.js
@@ -0,0 +1,14 @@
+/**
+ * @fileoverview getlatestfeeds
+ * @author SeasonLi | season.chopsticks@gmail.com
+ * @version 1.0 | 2016-03-19 | SeasonLi    // Initial version
+ */
+
+var engine = require('consolidate').mustache;
+
+
+module.exports = function (req, res) {
+  engine('../../view/page/index.html', {}, function (err, html) {
+    res.send(html);
+  });
+};
